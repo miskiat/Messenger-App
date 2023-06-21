0d0c4b14e7d9ad57d8fcba1af6dae6958c332612
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-06-22 00:57:01 +0100

    messengerlogo

---
+++ app/(site)/page.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div
+      className="
+    flex
+    min-h-full
+    flex-col
+    justify-center
+    py-12
+    sm:px-6
+    lg:px-8
+    bg-gray-100 
+    "
+    >
+      <div className="sm:mx-auto sm:m-full sm:max-w-md">
+        <image
+          alt="logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/messengerlogo.png"
+        />
+      </div>
+    </div>
+  );
+}
