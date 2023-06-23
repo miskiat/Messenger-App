ea6ef566c7cfe6888864ce659ef1a9082ccc655f
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-06-23 22:52:21 +0100

    usestate

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,20 @@
 "use client";
+
+import { useState } from "react";
+
+type Variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
-  return <div>Auth Form!</div>;
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallBack(() => {
+    if ((variant = "LOGIN")) {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, []);
+
+  return <div>Auth Formmm!</div>;
 };
 export default AuthForm;
