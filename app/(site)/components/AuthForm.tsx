3c5936aeb7d5abcfcd4fe444a2857937a1c71353
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-06-25 22:39:43 +0100

    usestate

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import { useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
-  const toggleVariant = useCallBack(() => {
+  const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
       setVariant("REGISTER");
     } else {
       setVariant("LOGIN");
     }
   }, [variant]);
-  const {} = useForm<FieldValues>({});
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+  };
 
   return <div>Auth Formmm!</div>;
 };
