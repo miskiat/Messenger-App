c855819b1a30eb3bfbb9d39b12aa05175803a596
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-06-30 22:06:15 +0100

    props

--- app/components/inputs/Button.tsx
+++ app/components/inputs/Button.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
 const Button = () => {
   return <div>Hello Button</div>;
 };
